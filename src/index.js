59e6b5d736349451ccd037cf9463db448715b120
Štěpán <stepan.mieres@seznam.cz>
2021-09-20 19:42:36 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+for (let i = 0; i < 1; i++) {
+    const array = ["Lodash", "React", "Next", "Strapi", "Axios", "Typescript"]
+    console.log(array)
+    array.reverse()
+    console.log(array)
+    array.sort()
+    console.log(array)
+    
+}     
+
+const car = {brand:"Ford", model:"mustang 750", year:"1969"};
+console.log(car)
+const randomcislo = Math.floor((Math.random() * 550) + 1);
+console.log("Náhodné číslo je: " + randomcislo)
+
+
+
